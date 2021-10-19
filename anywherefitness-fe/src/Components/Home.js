cdfb6c755c25451a77d85433014c79e9b611650d
Lia Moua <moua0061@yahoo.com>
2021-10-19 08:58:01 -1000

    added red box to see my divs to style

--- anywherefitness-fe/src/Components/Home.js
+++ anywherefitness-fe/src/Components/Home.js
@@ -6,11 +6,14 @@ export default function Home(){
     return (
         <div className='home-container'>
             <div className='content-container'>
-                <h3>Group fitness elevated</h3>
-                <h2>Workout With Us!</h2>
-                <p>Workout at your own time, WHENEVER you want and ANYWHERE you want. Either from Spain, Thailand, or Hawaii, take your workout with you anywhere in the globe.</p>
-                <div>
-                    <img src={workout1} alt='lady sit-up' className='workout1' />
+                <div className='text-content'>
+                    <h2>Group fitness elevated</h2>
+                    <h2>Workout With Us!</h2>
+                    <p>Workout at your own time, WHENEVER you want and ANYWHERE you want. Either from Spain, Thailand, or Hawaii, take your workout with you anywhere in the globe.</p>
+                </div>
+                
+                <div className='workout-img'>
+                    <img src={workout1} alt='lady sit-up' />
                 </div>
             </div>
         </div>
