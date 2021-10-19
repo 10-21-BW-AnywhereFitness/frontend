d506227b8a1b7aad5ef0e27f7f00a55d8a7ad3e2
Lia Moua <moua0061@yahoo.com>
2021-10-19 07:34:36 -1000

    added Link to logo and header

--- anywherefitness-fe/src/App.js
+++ anywherefitness-fe/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 import './App.css';
 import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <div className='logo-bar'>
+      <header className='header'>
+        <div className='logo-bar'>
+          <Link to='/home'>
             <h1>Anywhere Fitness</h1>
-      </div>      
+          </Link>
+        </div>     
+      </header>   
       <NavBar />
     </div>
   );
