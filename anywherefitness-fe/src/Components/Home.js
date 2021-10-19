32b89d6d38df92e32c9232d56e8e312ab514b9c6
Lia Moua <moua0061@yahoo.com>
2021-10-19 08:35:43 -1000

    working with home page

--- anywherefitness-fe/src/Components/Home.js
+++ anywherefitness-fe/src/Components/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import workout1 from './workout1.jpg'
 
 
 export default function Home(){
     return (
         <div className='home-container'>
             <div className='content-container'>
-            <p>lorem ipsum</p>
+                <h3>Group fitness elevated</h3>
+                <h2>Workout With Us!</h2>
+                <p>Workout at your own time, WHENEVER you want and ANYWHERE you want. Either from Spain, Thailand, or Hawaii, take your workout with you anywhere in the globe.</p>
+                <div>
+                    <img src={workout1} alt='lady sit-up' className='workout1' />
+                </div>
             </div>
         </div>
     )
