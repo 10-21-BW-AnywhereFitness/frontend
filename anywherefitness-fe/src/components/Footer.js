e018bcfa232b40c0a7f7d0650df09013a8274da8
Lia Moua <moua0061@yahoo.com>
2021-10-21 14:59:13 -1000

    fixing home page

--- anywherefitness-fe/src/components/Footer.js
+++ anywherefitness-fe/src/components/Footer.js
@@ -4,7 +4,7 @@ import { CardFooter } from 'reactstrap';
 export default function Footer(){
     return (
         <div className='contact-container'>
-            <CardFooter className='center fixed-bottom' >
+            <CardFooter className='center' >
             <p className="text-center">Copyright &copy; 2021 Anywhere Fitness</p>
             </CardFooter>
         </div>
--- anywherefitness-fe/src/components/pages/home.js
+++ anywherefitness-fe/src/components/pages/home.js
@@ -1,10 +1,14 @@
 import React from "react"
+import Workout from '../workout1.jpg'
+import { Image } from 'reactstrap';
 
 const Home = (props) =>{
     return (
         <div>
-            <h2>Home.js</h2>
-            <p>Home Page - public landing page</p>
+            <h2 className='text-center'>Welcome to Anywhere Fitness!</h2>
+                <div>
+                <p>Home landing page</p>
+                </div>
         </div>
     )
 }
